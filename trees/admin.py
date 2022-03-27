85be053120ceaf8fc0f467b240cde4abfa60021e
Joanna Gorska <gorska919@gmail.com>
2022-03-27 14:17:37 +0000

    Update models in trees app and admin and tested

--- trees/admin.py
+++ trees/admin.py
@@ -5,6 +5,28 @@ from django.contrib import admin
 from.models import Feature, Enviroment, Tree
 
 
-admin.site.register(Tree)
-admin.site.register(Feature)
-admin.site.register(Enviroment)
+@admin.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+    """
+    class to enable admin manage the Tree model
+    """
+    list_display = ('id', 'name', 'price', 'current_stock', 'slug')
+    prepopulated_fields = {'image_alt': ('name',)}
+    search_fields = ['name', 'description']
+
+
+@admin.register(Feature)
+class FeatureAdmin(admin.ModelAdmin):
+    """
+    class to enale admin manage Feature model
+    """
+    list_display = ('name', 'icon_fontawsome')
+
+
+@admin.register(Enviroment)
+class EnviromentAdmin(admin.ModelAdmin):
+    """
+    class to enable admin manage Enviroment model
+    """
+    list_display = ('name', 'icon_fontawsome')
+
--- trees/models.py
+++ trees/models.py
@@ -7,6 +7,15 @@ import random
 from django.db import models
 from django.template.defaultfilters import slugify
 
+IMG_DISCLAIMER_CHOICES = [
+        ('standarised', 'This is an image of a typical tree \
+                        of this size and shape. We grow and \
+                        sell many nearly identical specimen. \
+                        The product that you will purchase will be \
+                        very similar to the one on the photograph.'),
+        ('unique', 'This is an image of the specimen that is being for sale'),
+]
+
 
 class Feature(models.Model):
     """
@@ -54,7 +63,7 @@ class Tree(models.Model):
     """
     # these columns are required
     name = models.CharField(max_length=254, null=True, blank=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, editable=False)
     height_in = models.DecimalField(max_digits=9, decimal_places=2)
     trunk_width_in = models.DecimalField(max_digits=9, decimal_places=2)
     price = models.DecimalField(max_digits=9, decimal_places=2)
@@ -63,8 +72,11 @@ class Tree(models.Model):
     description = models.TextField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    image_description = models.CharField(max_length=254, null=True, blank=True)
-    image_disclaimer = models.TextField()
+    image_alt = models.CharField(max_length=254, null=True, blank=True)
+    image_disclaimer = models.CharField(
+            max_length=254, choices=IMG_DISCLAIMER_CHOICES,
+            default='standarised',  null=True, blank=True
+            )
     feature = models.ManyToManyField(
                 'Feature', blank=True)
     enviroment = models.ForeignKey(
@@ -72,20 +84,28 @@ class Tree(models.Model):
     # unique slug solution found on:
     # https://www.geeksforgeeks.org/add-the-slug-field-inside-django-model/
 
-    def random_string_generator(self, size=10, chars=string.ascii_lowercase + string.digits):
+    def random_string_generator(self,
+                                size=10,
+                                chars=string.ascii_lowercase + string.digits):
         """
-        generates a string of characters
+        generates a string of characters consisting of
+        lowercase letters and digits, lenght is set to 10
         """
         return ''.join(random.choice(chars) for _ in range(size))
 
     def save(self, *args, **kwargs):
+        """
+        automaticaly generates slug field
+        """
         if not self.slug:
             name_string = slugify(self.name)
-            random_characters = self.random_string_generator(size=6)
+            random_characters = self.random_string_generator()
             slug = f'{name_string}-{random_characters}'
+            # checks if this slug is already in the column slug
             if slug in self.slug:
-                random_characters = self.random_string_generator(size=6)
-                slug = f'{name_string}-{random_characters}'
+                random_characters = self.random_string_generator()
+                new_slug = f'{name_string}-{random_characters}'
+                self.slug = new_slug
             else:
                 self.slug = slug
         super(Tree, self).save(*args, **kwargs)
