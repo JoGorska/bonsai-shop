788d7ced4956a40e052167aa658096a843a205a7
Joanna Gorska <gorska919@gmail.com>
2022-03-31 17:27:23 +0000

    Add app trolley to act as a shopping bag for the user

--- bonsai_shop/settings.py
+++ bonsai_shop/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
     'home',
     'trees',
+    'trolley',
 
 ]
 
---
+++ trolley/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrolleyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'trolley'
