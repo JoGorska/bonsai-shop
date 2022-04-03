50da3d2b67a912da4af6f0d8136a01cd7b122fae
Joanna Gorska <gorska919@gmail.com>
2022-04-03 20:22:08 +0000

    Add urls views and template for checkout

--- bonsai_shop/urls.py
+++ bonsai_shop/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('trees/', include('trees.urls')),
     path('trolley/', include('trolley.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+"""
+views for checkout app
+"""
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    view to make checkout
+    """
+    trolley = request.session.get('trolley', {})
+    if not trolley:
+        messages.error(request,
+                       "There's nothing in your trolley at the moment")
+        return redirect(reverse('all_trees'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
