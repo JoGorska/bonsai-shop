45d9a7cb2adef30a90d010e6e6509266716f0cad
Joanna Gorska <gorska919@gmail.com>
2022-04-04 09:01:54 +0000

    Add payment intent and event listener on input for card errors

--- bonsai_shop/settings.py
+++ bonsai_shop/settings.py
@@ -184,6 +184,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# stripe
+
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
 
--- checkout/views.py
+++ checkout/views.py
@@ -4,25 +4,42 @@ views for checkout app
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
+import stripe
 
 from .forms import OrderForm
+from trolley.contexts import trolley_contents
 
 
 def checkout(request):
     """
     view to make checkout
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     trolley = request.session.get('trolley', {})
     if not trolley:
         messages.error(request,
                        "There's nothing in your trolley at the moment")
         return redirect(reverse('all_trees'))
 
+    current_trolley = trolley_contents(request)
+    total = current_trolley['grand_total']
+    stripe_total = round(total * 100)
+    # sets the secret key on stripe
+    stripe.api_key = stripe_secret_key
+    # creates payment intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(f'PAYMENT INTENT {intent}')
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'stripe_public_key': stripe_public_key,
         'client_secret': 'test client secret',
     }
 
