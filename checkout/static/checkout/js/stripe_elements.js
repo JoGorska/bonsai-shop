996d1ff325af3a0c955f6cd4eed874d5e2268aa1
Joanna Gorska <gorska919@gmail.com>
2022-04-04 09:41:54 +0000

    Add event listener for submit card payments and handle card errors

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -42,3 +42,37 @@ card.addEventListener('change', function (event) {
         errorDiv.textContent = '';
     }
 });
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    // disable card element to prevent multiple submittions
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    // submit payment to stripe
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            // show errors to customer
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // payment has been processed
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -36,11 +36,16 @@ def checkout(request):
     print(f'PAYMENT INTENT {intent}')
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test client secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
