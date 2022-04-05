24f75df5dd729f6244480e0e913f41b36ed5d2a3
Joanna Gorska <gorska919@gmail.com>
2022-04-05 19:57:30 +0000

    Made migrations for profile app

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.db.models import Sum
 from django.conf import settings
 
 from trees.models import Tree
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -12,7 +13,7 @@ class Order(models.Model):
     order model
     """
     order_number = models.CharField(max_length=32, null=False, editable=False)
-
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
 
     email = models.EmailField(max_length=254, null=False, blank=False)
