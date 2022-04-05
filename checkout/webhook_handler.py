f86a35131c00e4c9af954fcb2b0be483d923a67c
Joanna Gorska <gorska919@gmail.com>
2022-04-05 08:42:57 +0000

    Add try block in webhook handler to check if the order already exists

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from trees.models import Tree
+
 
 class StripeWHHandler:
     """Handle Stripe webhooks"""
@@ -19,6 +22,47 @@ class StripeWHHandler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        pid = intent.id
+        trolley = intent.metadata.trolley
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # Clean data in the shipping details
+        # replace any empty strings with value None
+        # stripe will store them as blank strings
+        # we want Null value in database
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+        try:
+            order_exists = False
+            # try to get object from the database, iexact - case insensitive
+            order = Order.objects.get(
+                        full_name__iexact=shipping_details.name,
+                        # iexact ignores the uppercase
+                        email__iexact=billing_details.email,
+                        phone_number__iexact=shipping_details.phone,
+                        country__iexact=shipping_details.address.country,
+                        postcode__iexact=shipping_details.address.postal_code,
+                        town_or_city__iexact=shipping_details.address.city,
+                        street_address1__iexact=shipping_details.address.line1,
+                        street_address2__iexact=shipping_details.address.line2,
+                        county__iexact=shipping_details.address.state,
+                        grand_total=grand_total,
+                        original_bag=bag,
+                        stripe_pid=pid,
+                    )
+            # breaks out of the loop if order exists
+            order_exists = True
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: verified order already in database',
+                status=200)
+
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
