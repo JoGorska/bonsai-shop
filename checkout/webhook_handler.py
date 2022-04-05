684ca939138e1b60dab349cb237f3569fa6fb473
Joanna Gorska <gorska919@gmail.com>
2022-04-05 09:40:42 +0000

    Add while loop with delay in webhook handler to make 5 attempts to check if order already exists before creating new order

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
+
 import json
+import time
+
 from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
@@ -39,31 +42,45 @@ class StripeWHHandler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
-        try:
-            order_exists = False
-            # try to get object from the database
-            # iexact - case insensitive
-            order = Order.objects.get(
-                        full_name__iexact=shipping_details.name,
-                        email__iexact=billing_details.email,
-                        phone_number__iexact=shipping_details.phone,
-                        country__iexact=shipping_details.address.country,
-                        postcode__iexact=shipping_details.address.postal_code,
-                        town_or_city__iexact=shipping_details.address.city,
-                        street_address1__iexact=shipping_details.address.line1,
-                        street_address2__iexact=shipping_details.address.line2,
-                        county__iexact=shipping_details.address.state,
-                        grand_total=grand_total,
-                        original_trolley=trolley,
-                        stripe_pid=pid,
-                    )
-            order_exists = True
-            return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: verified order already in database',
-                status=200)
 
-        except Order.DoesNotExist:
-            # creates a new order
+        order_exists = False
+        attempt = 1
+        # wile loop will make 5 attempts to check if order was already created
+        while attempt <= 5:
+            try:
+                # try to get object from the database
+                # iexact - case insensitive
+                order = Order.objects.get(
+                            full_name__iexact=shipping_details.name,
+                            email__iexact=billing_details.email,
+                            phone_number__iexact=shipping_details.phone,
+                            country__iexact=shipping_details.address.country,
+                            postcode__iexact=shipping_details.address.postal_code,
+                            town_or_city__iexact=shipping_details.address.city,
+                            street_address1__iexact=shipping_details.address.line1,
+                            street_address2__iexact=shipping_details.address.line2,
+                            county__iexact=shipping_details.address.state,
+                            grand_total=grand_total,
+                            original_trolley=trolley,
+                            stripe_pid=pid,
+                        )
+                # breaks out of the loop if order exists
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+
+        if order_exists:
+            # by this time order has definitely been completed
+            # if order exists returns 200 response
+            return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | SUCCESS: verified order already in database',
+                    status=200)
+
+        else:
+            # this is when the order form fails to be submited
+            # the webhook creates a new order from the data from stripe
             order = None
             try:
                 order = Order.objects.create(
