0fcae82eef72b1ceaf98409446f6c295a039654c
Joanna Gorska <gorska919@gmail.com>
2022-04-07 16:01:23 +0000

    Add post method for profile form

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -9,8 +10,16 @@ def profile(request):
     Display the user's profile
     """
     user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=user_profile)
     orders = user_profile.orders.all()
+    print(f'ORDERS {orders}')
     template = 'profiles/profile.html'
     context = {
         'form': form,
