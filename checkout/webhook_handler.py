e7ca004a1c7dfe26dac9998ba2912f9fa270a0d8
Joanna Gorska <gorska919@gmail.com>
2022-04-13 19:46:48 +0000

    Minor fix

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -23,6 +23,7 @@ class StripeWHHandler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
+        print(f'are you trying to send email? {cust_email}')
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
 
@@ -90,18 +91,18 @@ class StripeWHHandler:
                 # try to get object from the database
                 # iexact - case insensitive
                 order = Order.objects.get(
-                            full_name__iexact=shipping_details.name,
-                            email__iexact=billing_details.email,
-                            phone_number__iexact=shipping_details.phone,
-                            country__iexact=shipping_details.address.country,
-                            postcode__iexact=shipping_details.address.postal_code,
-                            town_or_city__iexact=shipping_details.address.city,
-                            street_address1__iexact=shipping_details.address.line1,
-                            street_address2__iexact=shipping_details.address.line2,
-                            county__iexact=shipping_details.address.state,
-                            grand_total=grand_total,
-                            original_trolley=trolley,
-                            stripe_pid=pid,
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_trolley=trolley,
+                    stripe_pid=pid,
                         )
                 # breaks out of the loop if order exists
                 order_exists = True
@@ -126,19 +127,19 @@ class StripeWHHandler:
             order = None
             try:
                 order = Order.objects.create(
-                            full_name=shipping_details.name,
-                            user_profile=profile,
-                            email=billing_details.email,
-                            phone_number=shipping_details.phone,
-                            country=shipping_details.address.country,
-                            postcode=shipping_details.address.postal_code,
-                            town_or_city=shipping_details.address.city,
-                            street_address1=shipping_details.address.line1,
-                            street_address2=shipping_details.address.line2,
-                            county=shipping_details.address.state,
-                            original_trolley=trolley,
-                            stripe_pid=pid,
-                        )
+                        full_name=shipping_details.name,
+                        user_profile=profile,
+                        email=billing_details.email,
+                        phone_number=shipping_details.phone,
+                        country=shipping_details.address.country,
+                        postcode=shipping_details.address.postal_code,
+                        town_or_city=shipping_details.address.city,
+                        street_address1=shipping_details.address.line1,
+                        street_address2=shipping_details.address.line2,
+                        county=shipping_details.address.state,
+                        original_trolley=trolley,
+                        stripe_pid=pid,
+                    )
 
                 for item_id, item_data in json.loads(trolley).items():
                     tree = Tree.objects.get(id=item_id)
