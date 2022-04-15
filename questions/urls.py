d5ee2442d993de62844dc636c147ff8be0b37cd5
Joanna Gorska <gorska919@gmail.com>
2022-04-15 13:07:57 +0000

    Add form template, view and url to add new questions

--- questions/urls.py
+++ questions/urls.py
@@ -8,4 +8,5 @@ from . import views
 
 urlpatterns = [
     path('', views.QuestionsList.as_view(), name='questions'),
+    path('add/', views.AddNewQuestion.as_view(), name='add_question'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -4,7 +4,10 @@ views for home app
 # pylint: disable=no-member
 from django.shortcuts import render
 from django.views import generic, View
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
 from .models import Question
+from .forms import QuestionForm
 
 
 class QuestionsList(generic.ListView):
@@ -15,3 +18,37 @@ class QuestionsList(generic.ListView):
     queryset = Question.objects.filter(status=1).order_by('-created_on')
     template_name = 'questions/questions.html'
     paginate_by = 10
+
+
+class AddNewQuestion(CreateView):
+    '''
+    class view in get - gets the traffic_msg_form and in post - posts the form
+    and creates new traffic alert
+    '''
+    template_name = 'questions/add_question.html'
+    form_class = QuestionForm
+    success_url = 'questions'
+
+    def get(self, request, *args, **kwargs):
+
+        return render(
+            request,
+            'questions/add_question.html',
+            {
+                'form': QuestionForm()
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+
+        form = QuestionForm(data=request.POST)
+        if form.is_valid():
+            form.instance.author_id = request.user.id
+
+            question = form.save(commit=False)
+            question.save()
+
+        else:
+            form = QuestionForm(instance=form)
+
+        return HttpResponseRedirect('questions')
