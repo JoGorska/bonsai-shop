5238efda394f3a57e60060df7064ea25a5dcc70f
Joanna Gorska <gorska919@gmail.com>
2022-04-16 11:54:50 +0000

    Add questions to be displayed on home page

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
 """
 views for home app
 """
+# pylint: disable=no-member
 from django.shortcuts import render
+from questions.models import Question
 
 
 def index_view(request):
     """
     view to return index page
     """
-    return render(request, 'home/index.html')
+    questions = Question.objects.filter(status=1).order_by('-created_on')
+    context = {
+        'questions': questions
+    }
+    return render(request, 'home/index.html', context)
