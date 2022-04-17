c560311f71e9f7cde7bd3191f2df16a0225480d7
Joanna Gorska <gorska919@gmail.com>
2022-04-17 09:22:11 +0000

    Add model and admin for newsletter app

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+"""
+admin for newsletter app
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Subscriber
+
+
+@admin.register(Subscriber())
+class SubscriberAdmin(admin.ModelAdmin):
+    '''
+    class enabling admin see the Subscriber model in admin panel
+    '''
+    list_display = ('email', 'registered_user', 'created_on',)
+    search_fields = ['email']
+    list_filter = ('accepted_privacy_policy', 'subscribed')
+
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,9 +1,25 @@
+"""
+Models for newsletter
+"""
+
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
-email
-accepted privicy policy
-user id or user foreign key Not reggistered shopper
-date of creation
-unsubscribed = False
+class Subscriber(models.Model):
+    """
+    creates a table of subscribers that have signed up to newsletter
+    """
+
+    email = models.EmailField(max_length=254, blank=False, null=False)
+    # user can set unsubscribed or can choose for this data to be deleted
+    subscribed = models.BooleanField(default=False, blank=False, null=False)
+    # I keep the record that they have actively accepted privacy policy
+    accepted_privacy_policy = models.BooleanField(default=False,
+                                                  blank=False, null=False)
+    # can I allow foreign key to be Null???
+    registered_user = models.ForeignKey(
+                User, on_delete=models.SET_NULL, blank=True, null=True,
+                related_name='newsletter_subscriber')
+    created_on = models.DateTimeField(auto_now=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
