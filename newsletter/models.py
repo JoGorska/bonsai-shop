c31452f591a66b974c53a743820c10e013635b20
Joanna Gorska <gorska919@gmail.com>
2022-04-17 08:23:52 +0000

    Add template for displaying signee list

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+email
+accepted privicy policy
+user id or user foreign key Not reggistered shopper
+date of creation
+unsubscribed = False
--- newsletter/views.py
+++ newsletter/views.py
@@ -9,4 +9,9 @@ def signees(request):
     """
     view to return list of people that have signed up for newsletter page
     """
+    
+
+    context = {
+        'signee_list': signee_list,
+    }
     return render(request, 'newsletter/signees.html')
