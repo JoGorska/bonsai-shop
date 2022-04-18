98711217fe2939935242d269dd220276f342107e
Joanna Gorska <gorska919@gmail.com>
2022-04-18 21:17:36 +0000

    Refactored add subscription view

--- newsletter/views.py
+++ newsletter/views.py
@@ -31,71 +31,138 @@ def add_subscriber(request):
 
     if request.method == 'POST':
         next_page = request.POST.get('next', '/')
-
-        email = request.POST.get("email")
+        email_from_form = request.POST.get("email")
+        email_to_lowercase = email_from_form.lower()
+        email = email_to_lowercase.strip()
+        print(f'EMAIL SHOULD BE LOWER CASE WITH NO SPACE{email}')
         all_subscribers = Subscriber.objects.all()
-        # checks if email in newsletter database
-        for registered_subscriber in all_subscribers:
-            if email == registered_subscriber.email:
-                # checks if email has status subscribed
-                if registered_subscriber.subscribed:
+        all_emails_subscribed = []
+        all_users_subscribed = []
+        for subscriber in all_subscribers:
+            all_emails_subscribed.append(subscriber.email)
+            all_users_subscribed.append(subscriber.registered_user)
+        print(f'TWO LISTS {all_emails_subscribed}, another one {all_users_subscribed}')
+        # checks if email is already in database
+        if email in all_emails_subscribed:
+            current_subscriber = Subscriber.objects.filter(email=email)
+            # email already in database and subscribed
+            if current_subscriber.subscribed:
+                messages.error(
+                        request,
+                        f'The email {email} is already in our database')
+                return HttpResponseRedirect(next_page)
+            # email in database, but currently unsubscribed
+            else:
+                current_subscriber.subscribed = True
+                current_subscriber.save(update_fields=['subscribed'])
+                messages.success(
+                    request,
+                    f'Successfully subscribed email {current_subscriber.email}\
+                        to our newsletter')
+                return HttpResponseRedirect(next_page)
+
+        # email is not in database
+        else:
+            # check if user details have been passed in POST request
+            if "registered_user" in request.POST:
+                current_user = request.POST.get("registered_user")
+                if current_user in all_users_subscribed:
+                    # check if this user has registered another email already
+                
+                    current_subscriber = Subscriber.objects.filter(
+                                                registered_user=current_user)
+                    if current_subscriber.subscribed:
+                        messages.error(
+                                request,
+                                f'The email {email} is already in our database')
+                        return HttpResponseRedirect(next_page)
+                    # email in database, but currently unsubscribed
+                    else:
+                        current_subscriber.subscribed = True
+                        current_subscriber.save(update_fields=['subscribed'])
+                        messages.success(
+                            request,
+                            f'Successfully subscribed email {current_subscriber.email}\
+                                to our newsletter')
+                        return HttpResponseRedirect(next_page)
+                else:
+                    # no user details in database, no email in database
+                    registered_user = current_user
+                    if "subscribed" in request.POST:
+                        subscribed = True
+                    else:
+                        subscribed = False
+
+                    if "accepted_privacy_policy" in request.POST:
+                        accepted_privacy_policy = True
+                    else:
+                        accepted_privacy_policy = False
+                    # creates instance of subscriber model class
+                    subscriber = Subscriber(
+                        email=email,
+                        subscribed=subscribed,
+                        accepted_privacy_policy=accepted_privacy_policy,
+                        registered_user=registered_user
+                    )
+                    if subscriber.subscribed is False:
+                        messages.error(
+                            request,
+                            'Please mark the option that you wish to subscribe\
+                            to newsletter')
+                        return HttpResponseRedirect(next_page)
+                    elif subscriber.accepted_privacy_policy is False:
+                        messages.error(
+                            request,
+                            'Please mark the option that you accept\
+                                 Privacy Policy')
+                        return HttpResponseRedirect(next_page)
+                    else:
+                        subscriber.save()
+                        messages.success(
+                            request,
+                            f'Subscribed email {subscriber.email}\
+                            to the newsletter')
+
+                        return HttpResponseRedirect(next_page)
+
+            else:
+                # no user details in request, no email in database
+                registered_user = None
+                if "subscribed" in request.POST:
+                    subscribed = True
+                else:
+                    subscribed = False
+
+                if "accepted_privacy_policy" in request.POST:
+                    accepted_privacy_policy = True
+                else:
+                    accepted_privacy_policy = False
+                # creates instance of subscriber model class
+                subscriber = Subscriber(
+                    email=email,
+                    subscribed=subscribed,
+                    accepted_privacy_policy=accepted_privacy_policy,
+                    registered_user=registered_user
+                )
+                if subscriber.subscribed is False:
                     messages.error(
                         request,
-                        'This email is already in our newsletter subscribers list')
+                        'Please mark the option that you wish to subscribe\
+                        to newsletter')
                     return HttpResponseRedirect(next_page)
-                # if email is in database, but has unsubscribed status
-                # gets this object and updates it to be subscribed
+                elif subscriber.accepted_privacy_policy is False:
+                    messages.error(
+                        request,
+                        'Please mark the option that you accept Privacy Policy')
+                    return HttpResponseRedirect(next_page)
                 else:
-                    already_in_database_subscriber = Subscriber.objects.get(email=email)
-                    already_in_database_subscriber.subscribed = False
-                    already_in_database_subscriber.save(update_fields=['subscribed'])
+                    subscriber.save()
                     messages.success(
                         request,
-                        f'Subscribed email {already_in_database_subscriber.email} to the newsletter')
+                        f'Subscribed email {subscriber.email}\
+                          to the newsletter')
+
                     return HttpResponseRedirect(next_page)
-
-        if "subscribed" in request.POST:
-            subscribed = True
-        else:
-            subscribed = False
-
-        if "accepted_privacy_policy" in request.POST:
-            accepted_privacy_policy = True
-        else:
-            accepted_privacy_policy = False
-
-        if "registered_user" in request.POST:
-            user_id = request.POST.get("registered_user")
-            registered_user = get_object_or_404(User, id=user_id)
-        else:
-            registered_user = None
-
-        # creates instance of subscriber class
-        subscriber = Subscriber(
-            email=email,
-            subscribed=subscribed,
-            accepted_privacy_policy=accepted_privacy_policy,
-            registered_user=registered_user
-        )
-
-        if subscriber.subscribed is False:
-            messages.error(
-                request,
-                'Please mark the option that you wish to subscribe\
-                 to newsletter')
-            return HttpResponseRedirect(next_page)
-        elif subscriber.accepted_privacy_policy is False:
-            messages.error(
-                request,
-                'Please mark the option that you accept Privacy Policy')
-            return HttpResponseRedirect(next_page)
-
-        messages.success(
-            request,
-            f'Subscribed email {subscriber.email} to the newsletter')
-        subscriber.save()
-
-        return HttpResponseRedirect(next_page)
 
 
 def unsubscribe(request):
@@ -114,7 +181,7 @@ def unsubscribe(request):
             messages.success(
                 request,
                 f'Successfully unsubscribed email {current_subscriber.email}\
-                    froum our newsletter')
+                    from our newsletter')
 
         except Subscriber.DoesNotExist:
             messages.error(
