853380d6e8d82187fbb0944a95132291e0dab70b
Joanna Gorska <gorska919@gmail.com>
2022-04-24 12:30:43 +0000

    Add colours to mobile nav bar depending on url

--- questions/urls.py
+++ questions/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('add/', views.add_question, name='add_question'),
     path('edit/<int:question_id>/', views.edit_question, name='edit_question'),
     path('delete/<int:question_id>/',
-          views.delete_question, name='delete_question'),
+         views.delete_question, name='delete_question'),
 ]
