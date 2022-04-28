b1bd443c898b4b34a44257c8270e00d9691023e1
Joanna Gorska <gorska919@gmail.com>
2022-04-28 23:30:14 +0000

    Docs: add issues to readme

---
+++ README_docs/tables.py
@@ -0,0 +1,8 @@
+
+def print_issues():
+    """function to pring issues for readme in a form of table"""
+    for i in range(140):
+        print(f'|  [#{i}](https://github.com/JoGorska/bonsai-shop/issues/{i}) |  |')
+
+
+print_issues()
