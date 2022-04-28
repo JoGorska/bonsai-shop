c0bfd5b3942164ca704cbb006632e8da0a1b5a1b
Joanna Gorska <gorska919@gmail.com>
2022-04-28 06:43:25 +0000

    Add reports from pep8 validation and fix pep8 errors

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,18 @@
+'''custom storages S3'''
+# pylint: disable=W0223
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
+    '''
+    set sup storage for static files
+    '''
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    '''
+    set sup storage for media files
+    '''
     location = settings.MEDIAFILES_LOCATION
--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,7 @@
 '''models for profiles app'''
 # pylint: disable=no-member
 # pylint: disable=C0103
+# pylint: disable=unused-argument
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
@@ -13,13 +14,18 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_country = models.CharField(default='GB', max_length=2, null=False, blank=False)
+    default_country = models.CharField(
+        default='GB', max_length=2, null=False, blank=False)
 
     def __str__(self):
         return f'{self.user.username}'
