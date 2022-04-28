56bb4a9850659f0422d9428859a52a125e881037
Joanna Gorska <gorska919@gmail.com>
2022-04-28 21:57:12 +0000

    Fix errors shown by jshint and upload documentation from jshint

--- trees/static/trees/js/trees.js
+++ trees/static/trees/js/trees.js
@@ -1,4 +1,5 @@
-
+/*jshint esversion: 6*/
+/*globals $:false */
 // On document ready hides all tiles for sorting and filtering
 // On click hides and shows appropriate tiles for sorting and filtering
 
@@ -14,19 +15,19 @@ $( document ).ready(function(){
         $(this).removeClass("text-white").addClass("text-success");
         $("#sorting-tile-wide").removeClass("text-success").addClass("text-white"); 
         $("#sorting-tile-mobile").removeClass("text-success").addClass("text-white");   
-    })
+    });
 
     $("#sorting-tile-wide").click(function(){
         $(".sorting-tile-wide").show();
         $(".feature-tile").hide();
         $(this).removeClass("text-white").addClass("text-success");
         $("#filter-tile").removeClass("text-success").addClass("text-white");  
-    })
+    });
     $("#sorting-tile-mobile").click(function(){
         $(".sorting-tile-mobile").show();
         $(".feature-tile").hide();
         $(this).removeClass("text-white").addClass("text-success"); 
         $("#filter-tile").removeClass("text-success").addClass("text-white"); 
-    })
+    });
 
-})
+});
